20827fa2f85e9e53e237df63f5d9618652d675e4
League Student <league@iMac-24.attlocal.net>
2019-10-13 11:28:29 -0700

    houses almost done

--- src/_01_houses/Houses.java
+++ src/_01_houses/Houses.java
@@ -1,5 +1,8 @@
 package _01_houses;
 
+import java.lang.ProcessBuilder.Redirect;
+import java.util.Random;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class Houses {
@@ -10,21 +13,37 @@ public static void main(String[] args) {
 	roby.setSpeed(150);
 	roby.penDown();
 	for (int i = 0; i < 10; i++) {
-drawHouse("small");
+Random randy=new Random();
+int num=randy.nextInt(9);
+if (num>6) {
+	drawHouse("large","BLUE");
+}
+else if (num<3 && num<=6) {
+	drawHouse("medium", "GREEN");
+}else if (num>0 && num<=3) {
+	drawHouse("small", "RED");
 }
 }
-static void drawHouse(String Height) {
+}
+static void drawHouse(String Height, String Color) {
+	int size=0;
 	if (Height.contentEquals("small")) {
+		size=60;
+		roby.setPenColor(java.awt.Color.RED);
 	}
 	if (Height.contentEquals("medium")) {
+		size=120;
+		roby.setPenColor(java.awt.Color.GREEN);
 	}	
 	if (Height.contentEquals("large")) {
+		size=250;
+		roby.setPenColor(java.awt.Color.BLUE);
 	}
-	roby.move(100);
+	roby.move(size);
 	roby.turn(90);
 	roby.move(20);
 	roby.turn(90);
-	roby.move(100);
+	roby.move(size);
 	roby.turn(270);
 	roby.move(20);
 	roby.turn(270);
