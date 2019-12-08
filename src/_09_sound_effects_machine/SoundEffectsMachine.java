0d7cc6885f11d997c4387bfdea341c177845a3c2
League Student <league@iMac-20.attlocal.net>
2019-12-08 11:28:46 -0800

    sound effects machine ip

--- src/_09_sound_effects_machine/SoundEffectsMachine.java
+++ src/_09_sound_effects_machine/SoundEffectsMachine.java
@@ -1,5 +1,19 @@
 package _09_sound_effects_machine;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 public class SoundEffectsMachine {
-
-}
+	public static void main(String[] args) {
+	JFrame frame=new JFrame();
+	JPanel panel=new JPanel();
+	JButton button=new JButton();
+	JButton thing=new JButton();
+	JButton square=new JButton();
+	panel.add(button);
+	panel.add(square);
+	panel.add(thing);
+	frame.setVisible(true);
+	frame.add(panel);
+	frame.setSize(500,500);
+}}
